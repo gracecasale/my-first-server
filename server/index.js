7f2f6498a548a19689d05f40a52f72e4d27d0f14
Grace Casale <gracecasale@gmail.com>
2018-11-17 14:52:19 -0500

    updated index.js to include new path with hello and name

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.envPORT || 4000;
 
 app.get('/', (request, response) => {
     response.status(200);
-    response.send('Yayy!! It works!');
+    response.send('YAYYY!!!!');
 });
+
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name
+    res.status(200);
+    res.json({
+        name: name,
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Now listening on port: ${port}`);
