33d9d8fcc2a6d807495c940b00a71568c03dbb36
Grace Casale <gracecasale@gmail.com>
2018-11-21 18:08:34 -0500

    set up client development script that will start the client sub project for us

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello this is the client!
+      </div>
+    );
+  }
+}
+
+export default App;
